934c900bb260c4eb12d2ce263efdcba661e6867f
7df <42803044+7df@users.noreply.github.com>
2019-01-13 11:26:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,9 +12,11 @@ client.login(process.env.BOT_TOKEN);
 
 
 
+
+
 client.on('ready',  () => {
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by tnt');
+  console.log('by safa7');
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
   console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
@@ -23,35 +25,12 @@ client.user.setStatus("dnd");
 });
 
 
-
-
-var anti_spam = require("discord-anti-spam");
- 
-anti_spam(bot, {
-  warnBuffer: 7,  //MHSTR
-  maxBuffer: 8,  //MHSTR
-  interval: 1000,  //MHSTR
-  warningMessage: "**سيتم طردك إن لم توقف سبام**",  //MHSTR
-  banMessage: "تم الطرد بسبب السبام",  //MHSTR
-  maxDuplicatesWarning: 7, //NAROX
-  maxDuplicatesBan: 10  //NAROX
-});
- 
-bot.login(process.env.BOT_SPAM);
- 
-const bot1 = new Discord.Client();
- 
- var anti_spam1 = require("discord-anti-spam");
-anti_spam1(bot1, {
-  warnBuffer: 7,  //@N.C™ » MHSTR#1119  //// حقوق مهستر
-  maxBuffer: 8,  //@N.C™ » MHSTR#1119
-  interval: 1000, //@N.C™ » MHSTR#1119
-  warningMessage: "**سيتم طردك إن لم توقف سبام**",  //NAROX codes
-  banMessage: "تم الطرد بسبب السبام",  //NAROX codes
-  maxDuplicatesWarning: 7,  //NAROX codes
-  maxDuplicatesBan: 10  //MHSTR
-});
-//// MHSTR 2K
- 
-//@N.C™ » MHSTR#1119
-////MHSTR 2K
+client.on('message' , message => {
+  var prefix = "$";
+  if(message.author.bot) return;
+  if(message.content.startsWith(prefix + "ping")) {
+ message.channel.send('Pong...').then((msg) => {
+      msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nDiscord API: ${Math.round(client.ping)} ms.\`\`\``);
+ })
+  }  
+ });
