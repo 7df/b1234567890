9c49fbe35ab95e5630938bcdf290a1f21cffa4a7
7df <42803044+7df@users.noreply.github.com>
2019-01-13 21:12:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,9 +129,9 @@ client.on('message', message => {
 
 
 [❖...........مسح الجات............❖]
- ❖$clear                ❖$مسح
+             ❖$clear                
 [❖...........الطرد...........❖]
-❖$kick                 ❖$طرد
+            ❖$kick                 
   
   **
   `);
@@ -164,72 +164,3 @@ client.on("message", message => {
 
 
 
-client.on("message", message => {
-              var prefix = "$";;
-              if (message.content.startsWith(prefix + "مسح")) {
-                  if(!message.channel.guild) return message.reply('**❌ ليس لديك رتبة ادمن **');         
-     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**🚫  MANAGE_MESSAGES ليس لديك رتبة ادمن او بريم 🚫 **');
-          var msg;
-          msg = parseInt(); ///tnt
-        
-        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error); ///tnt
-        message.channel.sendMessage("", {embed: { ///tnt
-          title: "** ➡ تــم مسح الشات ⬅ **",
-          color: 0x06DF00,
-          footer: { ///tnt
-            
-          }
-        }}).then(msg => {msg.delete(10000)});
-                            }
-  
-       
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-var prefix = "$"
-client.on('message', message => {
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);//thekings
-
-  if (command == "طرد") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");//thekings
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .kickable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");//thekings
-
-  message.guild.member(user).kick();//thekings
-
-  const kickembed = new Discord.RichEmbed()//thekings
-  .setAuthor(`KICKED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()//thekings
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({//thekings
-    embed : kickembed
-  })//thekings
-}
-});
