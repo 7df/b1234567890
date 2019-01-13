52d3e262120e13e5e0fda074ed5db29e5c3aff91
7df <42803044+7df@users.noreply.github.com>
2019-01-13 11:07:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,13 +28,13 @@ client.user.setStatus("dnd");
 var anti_spam = require("discord-anti-spam");
  
 anti_spam(bot, {
-  warnBuffer: 7,  
-  maxBuffer: 8,  
-  interval: 1000,  
-  warningMessage: "**سيتم طردك إن لم توقف سبام**",  
-  banMessage: "تم الطرد بسبب السبام",  
-  maxDuplicatesWarning: 7, 
-  maxDuplicatesBan: 10  
+  warnBuffer: 7,  //MHSTR
+  maxBuffer: 8,  //MHSTR
+  interval: 1000,  //MHSTR
+  warningMessage: "**سيتم طردك إن لم توقف سبام**",  //MHSTR
+  banMessage: "تم الطرد بسبب السبام",  //MHSTR
+  maxDuplicatesWarning: 7, //NAROX
+  maxDuplicatesBan: 10  //NAROX
 });
  
 bot.login(process.env.BOT_SPAM);
@@ -43,14 +43,15 @@ const bot1 = new Discord.Client();
  
  var anti_spam1 = require("discord-anti-spam");
 anti_spam1(bot1, {
-  warnBuffer: 7,  //@TnT » TnT#5855  /// TnT
-  maxBuffer: 8,  //@TnT » TnT#5855
-  interval: 1000, //@TnT » TnT#5855
-  warningMessage: "**سيتم طردك إن لم توقف سبام**",  
-  banMessage: "تم الطرد بسبب السبام",  
-  maxDuplicatesWarning: 7,  
-  maxDuplicatesBan: 10  
+  warnBuffer: 7,  //@N.C™ » MHSTR#1119  //// حقوق مهستر
+  maxBuffer: 8,  //@N.C™ » MHSTR#1119
+  interval: 1000, //@N.C™ » MHSTR#1119
+  warningMessage: "**سيتم طردك إن لم توقف سبام**",  //NAROX codes
+  banMessage: "تم الطرد بسبب السبام",  //NAROX codes
+  maxDuplicatesWarning: 7,  //NAROX codes
+  maxDuplicatesBan: 10  //MHSTR
 });
-
-
-
+//// MHSTR 2K
+ 
+//@N.C™ » MHSTR#1119
+////MHSTR 2K
