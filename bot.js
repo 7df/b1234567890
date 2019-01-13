f7c71bd587b4d13625e98240192540d5cffe826a
7df <42803044+7df@users.noreply.github.com>
2019-01-13 11:42:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,3 +34,26 @@ client.on('message' , message => {
  })
   }  
  });
+
+
+
+
+
+
+
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/)
+    if(message.content.includes('discord.gg')){
+        message.delete()
+      message.channel.sendMessage("", {embed: {
+        title: "لا تنشر",
+        color: 0x06DF00,
+        description: "يمنع النشر في هذا السيرفر",
+        footer: {
+          text: "By TnT"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+}); 
